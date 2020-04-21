a5e84a9ba7d692e3762a7dfcf9583d520e5ac272
Sasha Bogicevic <sasa.bogicevic@pm.me>
2020-04-21 23:20:50 +0200

    Initial config format

--- src/Fakie.hs
+++ src/Fakie.hs
@@ -5,11 +5,18 @@
 module Fakie where
 
 import qualified Control.Error          as ER
-import           Control.Exception.Safe (Exception, MonadThrow, throwM, tryAny)
+import           Control.Exception.Safe (Exception, MonadThrow, SomeException (..),
+                                         throwM, tryAny)
 import           Control.Monad.IO.Class (MonadIO, liftIO)
-import qualified Data.ByteString        as BS
+import           Data.Aeson
+import           Data.Aeson.TH
+import qualified Data.ByteString.Lazy   as BSL
+import           Data.Text              (Text)
+import qualified Data.Text              as T
+import           Network.HTTP.Types     (StdMethod)
 import           System.Directory       (getCurrentDirectory, listDirectory)
 import           System.FilePath        ((</>))
+import           Text.Casing            (camel)
 
 configFileName :: FilePath
 configFileName = ".fakie.conf"
@@ -18,23 +25,85 @@ newtype FakieException = FakieException String deriving (Show, Eq)
 
 instance Exception FakieException
 
-data FakieCfg =
-  FakieCfg
-    { fakieCfgRaw :: BS.ByteString
+data Method
+  = GET
+  | POST
+  | PUT
+  | DELETE
+  | HEAD
+  | OPTIONS
+  | PATCH
+  deriving (Eq, Show)
+
+$(deriveJSON defaultOptions ''Method)
+
+data FakieQueryParam =
+  FakieQueryParam
+    { fakieQueryParamName  :: Text
+    , fakieQueryParamValue :: Text
     } deriving (Eq, Show)
 
-readFakieConfig :: (MonadIO m, MonadThrow m) => m FakieCfg
+$(deriveJSON (defaultOptions { fieldLabelModifier = camel . drop 15 }) ''FakieQueryParam)
+
+data FakieHeader =
+  FakieHeader
+    { fakieHeaderName  :: Text
+    , fakieHeaderValue :: Text
+    } deriving (Eq, Show)
+
+$(deriveJSON (defaultOptions { fieldLabelModifier = camel . drop 11 }) ''FakieHeader)
+
+data FakieMap =
+  FakieMap
+    { fakieMapOurkey    :: Text
+    , fakieMapTheirkey  :: Text
+    , fakieMapFieldtype :: Text
+    } deriving (Eq, Show)
+
+$(deriveJSON (defaultOptions { fieldLabelModifier = camel . drop 8 }) ''FakieMap)
+
+data FakieItem =
+  FakieItem
+   { fakieItemName        :: Text
+   , fakieItemRoute       :: Text
+   , fakieItemMethod      :: Method
+   , fakieItemUrl         :: Text
+   , fakieItemQueryParams :: [FakieQueryParam]
+   , fakieItemHeaders     :: [FakieHeader]
+   , fakieItemMapping     :: [FakieMap]
+   } deriving (Eq, Show)
+
+$(deriveJSON (defaultOptions { fieldLabelModifier = camel . drop 9 }) ''FakieItem)
+
+data FakieApi =
+  FakieApi
+    { fakieApiItems :: [FakieItem]
+    } deriving (Eq, Show)
+
+$(deriveJSON (defaultOptions { fieldLabelModifier = camel . drop 8 }) ''FakieApi)
+
+type Fakie = [FakieItem]
+
+-- instance FromJSON Fakie where
+--   parseJSON = withArray "[]" $ \arr ->
+--     parseJSON arr
+
+-- $(deriveJSON defaultOptions ''Fakie)
+
+readFakieConfig :: (MonadIO m, MonadThrow m) => m Fakie
 readFakieConfig = do
-  eFakieConfigBS <- ER.runExceptT $ do
+  eFakie <- ER.runExceptT $ do
     cwd <- ER.ExceptT $ liftIO (tryAny getCurrentDirectory)
     liftIO $ putStrLn cwd
     fileList <- ER.ExceptT $ liftIO (tryAny $ listDirectory cwd)
     if configFileName `notElem` fileList
-      then throwM (FakieException "No config file detected")
+      then undefined -- throwM (SomeException "No config file detected")
       else do
         liftIO $ putStrLn (cwd </> configFileName)
-        fileContents <- ER.ExceptT $ liftIO $ tryAny (BS.readFile $ cwd </> configFileName)
-        return fileContents
-  case eFakieConfigBS of
-    Left err       -> throwM (FakieException $ show err)
-    Right rawBytes -> return (FakieCfg rawBytes)
+        fileContents <- ER.ExceptT $ liftIO $ tryAny (BSL.readFile $ cwd </> configFileName)
+        ER.hoistEither $ ER.fmapL (SomeException . FakieException) (eitherDecode fileContents :: Either String [FakieItem])
+  case eFakie of
+    Left (err :: SomeException) -> throwM (FakieException $ show err)
+    Right fakieConfig -> do
+      liftIO $ print fakieConfig
+      return fakieConfig
