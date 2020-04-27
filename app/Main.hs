9cfab18aec9ff565e12394889e8871fd2b826e82
Sasha Bogicevic <sasa.bogicevic@pm.me>
2020-04-27 22:01:58 +0200

    Add request headers and debug request function

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,34 @@
+{-# LANGUAGE NoImplicitPrelude   #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE PatternSynonyms     #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main where
 
-import           Debug.Pretty.Simple (pTraceShowM)
+import           Colog                    (pattern D, pattern I, Message,
+                                           pattern W, WithLog, cmap, fmtMessage,
+                                           log, logDebug, logInfo,
+                                           logTextStdout, logWarning,
+                                           usingLoggerT)
+import           Control.Concurrent.Async (mapConcurrently)
+import           Control.Exception.Safe   (SomeException)
+import           Control.Monad.Trans
+import qualified Data.Text                as T
+import           Debug.Pretty.Simple      (pTraceShowM)
 import           Fakie
+import           Prelude                  (Either (..), IO, putStrLn, return,
+                                           show, ($), (.))
 
 main :: IO ()
-main = do
-  putStrLn "Fakie Api server operational!"
-  config <- readFakieConfig
-  pTraceShowM config
+main = usingLoggerT logAction $ do
+  liftIO $ putStrLn "Fakie Api server started"
+  econfig <- readFakieConfig
+  case econfig of
+    Left (err :: SomeException) -> do
+      log W (T.pack . show $ err)
+      liftIO $ putStrLn "Fakie config could not be obtained. More info is in the log files"
+    Right fakieConfig -> do
+      v <- liftIO $ mapConcurrently callApi fakieConfig
+      return ()
+  where
+    logAction = cmap fmtMessage logTextStdout
--- src/Fakie.hs
+++ src/Fakie.hs
@@ -1,42 +1,60 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RankNTypes        #-}
-{-# LANGUAGE RecordWildCards   #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE NoImplicitPrelude          #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE PatternSynonyms            #-}
+{-# LANGUAGE RankNTypes                 #-}
+{-# LANGUAGE RecordWildCards            #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE StandaloneDeriving         #-}
+{-# OPTIONS_GHC -fno-warn-orphans       #-}
 
 module Fakie where
 
-import           Control.Concurrent.Async (mapConcurrently)
+import           Blaze.ByteString.Builder (Builder, fromByteString,
+                                           fromLazyByteString, toLazyByteString)
+import           Colog                    (pattern D, pattern I, LoggerT (..),
+                                           Message, WithLog, cmap, fmtMessage,
+                                           log, logTextStdout, usingLoggerT)
 import qualified Control.Error            as ER
 import           Control.Exception.Safe   (MonadThrow, SomeException (..),
                                            throwM, tryAny)
 import           Control.Monad            (when)
 import           Control.Monad.IO.Class   (MonadIO, liftIO)
 import           Data.Aeson
+import qualified Data.ByteString          as BS
 import qualified Data.ByteString.Lazy     as BSL
 import qualified Data.CaseInsensitive     as CI
+import           Data.Int                 (Int64)
 import qualified Data.Text                as T
-import           Data.Text.Encoding       (encodeUtf8)
-import           Network.HTTP.Client      (parseUrlThrow)
+import           Data.Text.Encoding       (decodeUtf8, encodeUtf8)
+import           Debug.Pretty.Simple      (pTraceShow)
+import           Network.HTTP.Client      (GivesPopper, Request (..),
+                                           RequestBody (..), parseUrlThrow)
 import           Network.HTTP.Simple
+import           Prelude                  (Either (..), Maybe (..), String,
+                                           error, fromIntegral, notElem, print,
+                                           return, show, ($), (.), (/=), (<$>),
+                                           (<>))
 import           System.Directory         (getCurrentDirectory, listDirectory)
 import           System.FilePath          ((</>))
 import           Types
 
-readFakieConfig :: (MonadIO m, MonadThrow m) => m Fakie
-readFakieConfig = do
-  eFakie <- ER.runExceptT $ do
+deriving instance MonadThrow m => MonadThrow (LoggerT Message m)
+
+readFakieConfig :: (WithLog env Message m, MonadIO m, MonadThrow m) => m (Either SomeException Fakie)
+readFakieConfig =
+  ER.runExceptT $ do
     cwd <- ER.ExceptT $ liftIO (tryAny getCurrentDirectory)
     fileList <- ER.ExceptT $ liftIO (tryAny $ listDirectory cwd)
     if configFileName `notElem` fileList
-      then throwM (FakieException "No config file detected")
+      then do
+        throwM (FakieException "No config file detected")
       else do
         fileContents <- ER.ExceptT $ liftIO $ tryAny (BSL.readFile $ cwd </> configFileName)
-        ER.hoistEither $ ER.fmapL (SomeException . FakieException) (eitherDecode fileContents :: Either String [FakieItem])
-  case eFakie of
-    Left err -> throwM (FakieException $ show err)
-    Right fakieConfig -> do
-      v <- liftIO $ mapConcurrently callApi fakieConfig
-      liftIO $ print v
-      return fakieConfig
+        ER.hoistEither $
+          ER.fmapL (SomeException . FakieException) (eitherDecode fileContents :: Either String [FakieItem])
 
 callApi :: (MonadIO m, MonadThrow m) => FakieItem -> m Value
 callApi fItem@FakieItem {..} = liftIO $ do
@@ -47,8 +65,10 @@ callApi fItem@FakieItem {..} = liftIO $ do
     Left err -> throwM (FakieException (show err))
     Right request'' -> do
       let request' =
-            setRequestQueryString (constructQueryParams fItem) request''
+            setRequestQueryString (constructQueryParams fItem) $
+            setUpRequestHeaders request'' fItem
           request = setUpRequestBody request' fItem
+      debugRequestContents request
       response <- httpJSONEither request
       case getResponseBody response :: Either JSONException Value of
         Left err -> throwM (FakieException (show err))
@@ -57,11 +77,45 @@ callApi fItem@FakieItem {..} = liftIO $ do
             throwM (FakieException "Received non 200 status code!")
           return rBody
 
+debugRequestContents :: MonadIO m => Request -> m ()
+debugRequestContents request = usingLoggerT logAction $ do
+  log D "Request"
+  log D "Host:"
+  log D (decodeUtf8 . host $ request)
+  log D "Port:"
+  log D (T.pack . show . port $ request)
+  log D "Method:"
+  log D (decodeUtf8 . method $ request)
+  log D "Path:"
+  log D (decodeUtf8 . path $ request)
+  log D "Query string:"
+  log D (decodeUtf8 . queryString $ request)
+  log D "Secure:"
+  log D (T.pack . show . secure $ request)
+  log D "Headers:"
+  log D (T.pack . show $ requestHeaders request)
+  let body' = requestBody request
+  case simplify body' of
+    Left (_, builder) -> do
+      log D "Body:"
+      log D (decodeUtf8 . BSL.toStrict $ toLazyByteString builder)
+      return ()
+    Right _ -> do
+      log D "No support for streaming request bodies for now"
+      return ()
+  where
+    logAction = cmap fmtMessage logTextStdout
+    simplify :: RequestBody -> Either (Int64, Builder) (Maybe Int64, GivesPopper ())
+    simplify (RequestBodyLBS lbs) = Left (BSL.length lbs, fromLazyByteString lbs)
+    simplify (RequestBodyBS bs) = Left (fromIntegral $ BS.length bs, fromByteString bs)
+    simplify (RequestBodyBuilder len b) = Left (len, b)
+    simplify _ = error "Not interested in streaming bodies right now"
+
 setUpRequestBody :: Request -> FakieItem -> Request
 setUpRequestBody r FakieItem {..} =
   case fakieItemBody of
     Nothing   -> r
-    Just body -> setRequestBodyLBS (encode body) r
+    Just body -> setRequestBodyJSON body r
 
 setUpRequestHeaders :: Request -> FakieItem -> Request
 setUpRequestHeaders r FakieItem {..} =
